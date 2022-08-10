61094df442e171becb3712b2103c25014a24cc83
ec965 <22567131+ec965@users.noreply.github.com>
2022-08-10 14:48:02 -0700

    Finish astro prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
