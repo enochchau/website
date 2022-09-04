25e8e6f8b24a4a12ba54eb22133c47316dd4ed97
ec965 <22567131+ec965@users.noreply.github.com>
2022-09-03 23:04:45 -0700

    Refactor contact hide/show into solid component

---
+++ src/components/ContactButton.tsx
@@ -0,0 +1,22 @@
+/** @jsxImportSource solid-js */
+import "solid-js";
+import { createSignal, Show } from "solid-js";
+
+export default function ContactButton() {
+  const [show, setShow] = createSignal(false);
+
+  return (
+    <Show
+      when={show()}
+      fallback={
+        <button onclick={() => setShow((show) => !show)}>
+          Show Contact
+        </button>
+      }
+    >
+      <p>
+        <a href="mailto: enoch965@gmail.com">enoch965@gmail.com</a>
+      </p>
+    </Show>
+  );
+}
