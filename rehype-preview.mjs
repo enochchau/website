9c4446b1ccfb1a2f5fd97afde6133a68a212c4fd
ec965 <22567131+ec965@users.noreply.github.com>
2022-10-26 19:33:04 -0700

    rm console log

--- rehype-preview.mjs
+++ rehype-preview.mjs
@@ -2,7 +2,6 @@ import { toHtml } from "hast-util-to-html";
 
 export function rehypePreview() {
   return function (tree, { data }) {
-    console.log(tree);
     data.astro.frontmatter.preview = toHtml(
       tree.children.filter((node) => !/^mdx/.test(node.type)).slice(0, 3)
     );
