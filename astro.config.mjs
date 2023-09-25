90d5084a1f1e7e37949e530844cf29cd51879366
ec965 <enoch965@gmail.com>
2023-09-25 02:18:26 -0700

    migrate astro to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import solid from "@astrojs/solid-js";
 import { defineConfig } from "astro/config";
@@ -8,19 +7,12 @@ import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    solid(),
-  ],
+  integrations: [mdx(), solid()],
   markdown: {
     gfm: true,
     shikiConfig: {
       theme: "one-dark-pro",
     },
-    extendDefaultPlugins: true,
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [rehypePreview],
   },
