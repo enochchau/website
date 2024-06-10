17bb54d790bc78f8ef25c43879d1623b629457e5
ec965 <enoch965@gmail.com>
2024-06-10 00:19:15 -0700

    src/components/Mascot.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,35 @@
+import typography from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: ['selector'],
-	theme: {
-		extend: {},
-		colors: {
-			white: '#f0f0f0',
-			black: '#1a1b1d',
-			purple: {
-				500: '#a86be7',
-				600: '#6f1cc5'
-			},
-			gray: {
-				200: "#dddddd",
-				300: '#a2a3a8',
-				400: '#8f96a1',
-				500: '#666666',
-				600: '#444444'
-			},
-			transparent: 'transparent'
-		}
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["selector"],
+  theme: {
+    extend: {
+      typeography: ({theme}) => ({
+        DEFAULT: {
+          css: {
+
+          }
+        }
+
+      })
+    },
+    colors: {
+      white: "#f0f0f0",
+      black: "#1a1b1d",
+      purple: {
+        500: "#a86be7",
+        600: "#6f1cc5",
+      },
+      gray: {
+        200: "#dddddd",
+        300: "#a2a3a8",
+        400: "#8f96a1",
+        500: "#666666",
+        600: "#444444",
+      },
+      transparent: "transparent",
+    },
+  },
+  plugins: [typography()],
+};
