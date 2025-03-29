2e6bbeefddf5cf9d9cdb9489a70ece16c048162e
ec965 <enoch965@gmail.com>
2025-03-28 22:33:22 -0700

    testing out css box shadow for highlighting anchors

--- src/components/BlogHeader.tsx
+++ src/components/BlogHeader.tsx
@@ -10,6 +10,7 @@ export default function BlogHeader(props: { children?: JSX.Element }) {
         ["w-fit relative text-xl font-bold blog-title"]: true,
         "before:w-full before:bg-purple-500/30 before:absolute before:h-1 before:bottom-1 hover:before:h-2/3 before:rounded-sm before:transition-all before:px-1 before:box-content before:-left-1":
           true,
+        // 'rise-anchor': true
       }}
     >
       {props.children}
