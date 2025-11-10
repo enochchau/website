d5b75f07ca4ab611f27287c78768e3d80851c403
enochchau <enoch965@gmail.com>
2025-11-09 23:44:36 -0800

    update lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginAstro from "eslint-plugin-astro";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
 
