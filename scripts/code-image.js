25dbe9acc20aa1a3bc52a17e5aff1a588e091a74
enochchau <enoch965@gmail.com>
2026-03-25 00:19:59 -0700

    use code preview as filename for code images

--- scripts/code-image.js
+++ scripts/code-image.js
@@ -2,7 +2,14 @@ import { execSync } from "node:child_process";
 import fs from "node:fs/promises";
 import path from "node:path";
 
+import sanitize from "sanitize-filename";
 import { codeToImage } from "shiki-image";
+
+const SHIKI_THEME = "one-dark-pro";
+
+/**
+ * TODO: Use a nerd-font
+ */
 
 const repoRoot = execSync("git rev-parse --show-toplevel").toString().trim();
 const blogDir = path.join(repoRoot, "src/content/blog");
@@ -22,6 +29,13 @@ await Promise.all(promises);
 async function createCodeImageForMdx(filePath) {
   let file = await fs.readFile(filePath);
   let content = file.toString();
+  const hasCover = /^---\s*\n[\s\S]*?\ncover:\s*\S+[\s\S]*?\n---/m.test(
+    content,
+  );
+
+  // skip generation if there's already a cover image
+  if (hasCover) return;
+
   const codeBlockRe = /^```([a-z-]*)\n([\s\S]*?)\n```/gm;
 
   let i = 0;
@@ -32,12 +46,14 @@ async function createCodeImageForMdx(filePath) {
     let buf = await codeToImage(code, {
       lang,
       format,
-      theme: "one-dark-pro",
+      theme: SHIKI_THEME,
     });
-
-    const fileName = `${filePath.replace(/\.mdx$/, "")}-${String(i).padStart(3, "0")}.${format}`;
+    let preview = sanitize(code.slice(0, 30).replace(/\s/g, "")).slice(0, 12);
+    const fileName = path.join(
+      path.dirname(filePath),
+      `code-image-${String(i).padStart(3, "0")}-${lang}-${preview}.${format}`,
+    );
     fs.writeFile(fileName, buf);
-
     i++;
   }
 }
