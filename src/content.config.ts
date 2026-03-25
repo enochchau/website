39dac718e7591a9ccc4b8a02de0ff58a0e247379
enochchau <enoch965@gmail.com>
2026-03-24 22:46:47 -0700

    add cover for og:image

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,13 +4,15 @@ import { defineCollection } from "astro:content";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog" }),
-  schema: z.object({
-    draft: z.boolean().optional(),
-    title: z.string(),
-    date: z.string(),
-    subtitle: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      draft: z.boolean().optional(),
+      title: z.string(),
+      date: z.string(),
+      subtitle: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+      cover: image().optional(),
+    }),
 });
 
 const test = defineCollection({
